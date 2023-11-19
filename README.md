508d4610f5f537a2b031c19453678bccacb5b6cd
JirachiBot <148644406+JirachiBot@users.noreply.github.com>
2023-11-19 07:38:05 +0530

    Added "Safemode"

--- README.md
+++ README.md
@@ -8,7 +8,6 @@
 - Self-botting is against Pokétwo and even Discord's TOS
 - If you get banned for using this bot, it is not my responsibility
 - It is recommended to use you alt rather than your main as the bot
-- Make sure your `config.json` file isn't public!
 
 ## Sup, this is a discord selfbot that responds to the Pokename bot's pings
 You will need to enter some values in config.json, (make sure these are private!) They are:
@@ -36,11 +35,13 @@ after entering your values, it should look something like this: https://imgur.co
 - logs when you catch your hunt
 - logs when you catch a shiny pokemon and sends a (kinda) realistic response when it catches a shiny pokemon, like "nice"
 - No need to complete any CAPTCHAs
+- Safe mode (can be enabled both automatically and manually) to stop catching
 
 ## Command(s):
 
 - **sh**: shows the bot's current shinyhunt streak (use in a private server)
 - **say**: says whatever you want the bot to say
+- **safemode**: toggleable command, enabling safemode stops your bot from catching pokemon for 5 minutes
 
 ## Setup:
 
@@ -51,4 +52,4 @@ after entering your values, it should look something like this: https://imgur.co
 
 `Happy Catching!`
 
-`If you need any help, you can join https://www.guilded.gg/JirachiBot !
+If you need any help, you can join https://www.guilded.gg/JirachiBot !
--- index.js
+++ index.js
@@ -19,12 +19,14 @@ const Hunt = settings.Hunt;
 const Pname = "874910942490677270";
 const P2 = "716390085896962058";
 var gg = new Array('gg', 'nice', 'cool', 'wow', 'thanks', 'lol');
+var sm = false;
 
+//0. Bot On and status
 client.on("ready", () => {
-  console.log(`​\nBot Created by @JirachiBot\nInspired/Forked from @verysussy's "24/7 Dc Spem bot"\n​\nBot is ready!`);
+  console.log(`​\nBot Created by @JirachiBot\nInspired/Forked from @verysussy's "24/7 Dc Spem bot"\n​\nBot (@${client.user.username}) is ready!`);
   client.channels.fetch(logID)
     .then(channel => {
-      channel.send(`Bot is On!`)
+      channel.send("> # **Bot is On!**")
     })
     client.user.setStatus("invisible");
       accountCheck = client.user.username;
@@ -37,20 +39,39 @@ client.on('message', msg => {
   let args = msg.content.toLowerCase().slice(prefix.length).trim().split(" ");
   let cmd = args.shift();
   if (cmd == "sh") {
-    //msg.delete(); //[Deletes the message you sent, needs "Manage Messages" perm. Delete the "//" at the beginning to enable, do not touch if you don't understand :P]
+    //msg.delete();
       return msg.channel.send(`<@${P2}> sh`)
   }
 });
 
-//2. Pokename's shinyhunt ping response
+//2. Pokename's shinyhunt ping response + Log/Safemode toggle
 client.on('message', msg => {
   if (!msg.content.startsWith("**✨Shiny Hunt Pings:** ")) return;
   if (!msg.content.includes(`<@${botID}>`)) return;
   if (msg.author.id != Pname) return;
-    setTimeout(function(){
-      return msg.channel.send(`<@${P2}> c ${Hunt}`)
-    },1000 * 2.83 ) 
+
+  setTimeout(function() {
+    if (sm == false) {
+      msg.channel.send(`<@${P2}> c ${Hunt}`).then(() => {
+        const collector = new Discord.MessageCollector(msg.channel, m => m.author.id === P2 && m.content.includes("+1 Shiny chain!") && m.content.includes(`<@${botID}>`), { time: 1000 * 5 });
+        collector.on('collect', response => {
+          client.channels.cache.get(logID).send(`> ### +1 ${Hunt} caught!`);
+          collector.stop();
+        });
+
+        collector.on('end', collected => {
+          if (collected.size === 0) {
+            client.channels.cache.get(logID).send(`${prefix}safemode true`);
+            setTimeout(function(){
+            client.channels.cache.get(logID).send(`> ### **<@${ownerID}> Your bot has gone into safe mode, please check you bot (${client.user.username})**`);
+          },1000 * 1 ) 
+          }
+        });
+      });
+    }
+  }, 1000 * 3 );
 });
+
 
 //3. Pokename's collection ping response
 client.on('message', msg => {
@@ -58,22 +79,12 @@ client.on('message', msg => {
   if (!msg.content.includes(`<@${botID}>`)) return;
   if (msg.author.id != Pname) return;
     setTimeout(function(){
+      if (sm == false)
       return msg.channel.send(`<@${P2}> c ${Hunt}`)
-    },1000 * 2.84 ) 
+    },1000 * 3 ) 
 });
 
-//4. +1 Streak log
-client.on('message', msg => {
-  if (!msg.content.includes("+1 Shiny chain!")) return;
-  if (!msg.content.includes(`<@${botID}>`)) return;
-  if (msg.author.id != P2) return;
-    client.channels.fetch(logID)
-    .then(channel => {
-      channel.send(`+1 ${Hunt} caught!`)
-    })
-});
-
-//5. Shiny caught log + response
+//4. Shiny caught log + response
 client.on('message', msg => {
   if (!msg.content.includes("These colors seem unusual...")) return;
   if (!msg.content.includes(`<@${botID}>`)) return;
@@ -83,21 +94,45 @@ client.on('message', msg => {
   },1000 * 1.57 )
     client.channels.fetch(logID)
     .then(channel => {
-      channel.send(`Shiny Hunt Caught! ||<@${ownerID}>||`)
+      channel.send(`> # **Shiny ${Hunt} Caught! ||<@${ownerID}>||**`)
     });
 });
 
-//6. Say command
+//5. Say command
 client.on('message', msg => {
   if (!msg.content.startsWith(prefix)) return;
   if (msg.author.id != ownerID) return;
   let args = msg.content.toLowerCase().slice(prefix.length).trim().split(" ");
   let cmd = args.shift();
-  if (cmd === 'say') {
+  if (cmd === "say") {
    const sayMessage = msg.content.split(' ').slice(1).join(' ');
     //msg.delete();
    msg.channel.send(sayMessage);
  }
+});
+//6. Safe mode toggle
+client.on('message', msg => {
+  if (!msg.content.startsWith(prefix)) return;
+  if (msg.author.id != ownerID && msg.author.id != botID) return;
+  let args = msg.content.toLowerCase().slice(prefix.length).trim().split(" ");
+  let cmd = args.shift();
+  if (cmd == "safemode") {
+    if (args[0] === 'true') {
+      sm = true;
+      msg.react("✅");
+
+      setTimeout(function() {
+        if (sm == true)
+        sm = false;
+        msg.channel.send("> Safemode set to `FALSE` automatically after `5` minutes");
+      }, 1000 * 60* 5);
+    } else if (args[0] === 'false') {
+      sm = false;
+      msg.react("☑️");
+    } else {
+      msg.channel.send("> Safemode is currently set to " + "`" + `${sm}` + "`\n> " + `\n> type ${prefix}safemode ` + "`TRUE/FALSE` to toggle.");
+    }
+  }
 });
 
 client.login(settings.token);
